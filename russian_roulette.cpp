5a0fb3698702bd0206f24981fe09c115802ca482
EkleZiast <149783990+EkleZiastX@users.noreply.github.com>
2024-04-25 21:33:51 +0200

    Add files via upload

---
+++ russian_roulette.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0)); 
+
+    cout << "Welcome to russian roulette" << endl;
+    while (1 < 2) {
+        cout << "Press Enter, for shot..." << endl;
+        cin.get(); 
+
+        int chamber = rand() % 6 + 1; 
+
+        if (chamber == 1) {
+            cout << "Congratulations! you survived!" << endl;
+        }
+        else {
+            cout << "Alas, you lost... The bullet will shoot!" << endl;
+        }
+
+    }
+    return 0;
+}
+// 5 bullets :)
